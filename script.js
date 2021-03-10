9bc74e4627f99d84783088f312a5310cd33c0215
Cindy Li <cindyl5697@hstat.org>
2021-03-10 23:44:05 +0000

    complete the base-objects,prompt,loop

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/jvce-szsb.json',
     dataType: 'json',
     success: function(data) {
-        console.log(data);
+    var usersStore = prompt("What type of store would you like to visit?");
+    for(var i = 0; i < data.length; i++){
+        
+      if (data[i].type == usersStore) {
+        document.write(data[i].store + "<br>");
     }
+}
+
+}
 });
